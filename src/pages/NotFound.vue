f80a213b44be7ae411c66274741489759e7f2604
Fatma Mohamed <63647644+Fatma-M@users.noreply.github.com>
2022-10-02 16:46:22 +0200

    Add files via upload

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        this page could not be found - check out all our
+        <router-link to="/coaches">Coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
